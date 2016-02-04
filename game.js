19c34ab9025e49d31642f21566919e57583aa0e1
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-02-04 15:47:03 -0200

    new images

--- game.js
+++ game.js
@@ -22,12 +22,20 @@ var images = {
         'http://imagem.band.com.br/f_247152.jpg',
         'http://1.bp.blogspot.com/-WqlbfWAhPmA/UkIFg1G_gEI/AAAAAAAAMuU/66Eg6nk53LM/s1600/fortunati_Regina_Becker_galinha.jpg',
         'http://zh.rbsdirect.com.br/imagesrc/16840789.jpg?w=640',
+        'http://www.sul21.com.br/wp-content/uploads/2014/11/20141125-por-filipe-castilhos-_mg_0211.jpg',
+        'images/11116088_858789557527136_829399231_n.jpg',
+        'images/11118346_858789587527133_670369225_n.jpg',
+        'images/11119823_858789804193778_1890904235_n.jpg',
+        'images/11158077_858789867527105_616142091_n.jpg',
+        'images/11169036_858789820860443_781564001_n.jpg',
     ],
     storm: [
         'http://multimidia.correiodopovo.com.br/thumb.aspx?Caminho=multimidia/2016/01/31/382544.JPG&Tamanho=1024',
         'http://s.glbimg.com/jo/g1/f/original/2016/02/01/img_1604.jpg',
         'http://www.correiodobrasil.com.br/wp-content/uploads/2016/02/chuva.jpg',
         'http://www.mancheteonline.com.br/wp-content/uploads/2016/02/Porto-Alegre-Chuva.jpg',
+        'http://s2.glbimg.com/6-YrD29CvKv7TUAgEd3ZBg1Xjtk=/620x465/s.glbimg.com/jo/g1/f/original/2016/02/01/img_1230.jpg',
+        'http://s.glbimg.com/jo/g1/f/original/2016/01/31/img_0656.jpg',
     ],
     combined: []
 };
